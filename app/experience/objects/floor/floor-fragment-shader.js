04ab150f240521f53035768806b364f0061265a5
Jack Wild <jack.wild@live.co.uk>
2017-01-17 18:09:08 +0000

    blah

--- app/experience/objects/floor/floor-fragment-shader.js
+++ app/experience/objects/floor/floor-fragment-shader.js
@@ -5,19 +5,19 @@ const FRAGMENT_SHADER =
 varying vec3 vNormal;
 varying vec3 vPosition;
 float maxOffsetPosition = ${FLOOR_MAX_OFFSET}.0;
-vec3 colorOne = vec3(151.0 / 255.0, 113.0 / 255.0, 173.0 / 255.0);
-vec3 colorTwo = vec3(184.0 / 255.0, 151.0 / 255.0, 101.0 / 255.0);
+vec3 colorOne = vec3(34.0 / 255.0, 41.0 / 250.0, 183.0 / 255.0);
+vec3 colorTwo = vec3(50.0 / 255.0, 195.0 / 255.0, 237.0 / 255.0);
 
 void main() {
 	vec3 light = vec3(0.5, 0.2, 1.0);
 	light = normalize(light);
 
 	float dProd = max(0.0, dot(vNormal, light));
-	dProd /= 2.0;
-	dProd += 0.5;
+	// dProd /= 2.0;
+	// dProd += 0.5;
 	float control = (vPosition.z + maxOffsetPosition) / (maxOffsetPosition * 2.0);
 	vec3 color = mix(colorOne, colorTwo, control);
-	color *= dProd;
+	// color *= dProd;
 
 	gl_FragColor = vec4(color, 1.0);
 }
