a7aa35a490360fef3bac6e59036a76d3e6807ff0
Jack Wild <jack.wild@live.co.uk>
2018-02-02 09:58:13 +0000

    noise working pretty well

--- app/experience/InstancedParticles.js
+++ app/experience/InstancedParticles.js
@@ -24,9 +24,9 @@ const InstancedParticles = () => {
 	let renderTarget1, renderTarget2, originsTexture, simulationMaterial;
 
 	for (let i = 0; i < INSTANCES; i++) {
-		const oX = (Math.random() - 0.5) * 1000;
-		const oY = (Math.random() - 0.5) * 2000;
-		const oZ = (Math.random() - 0.5) * 1000;
+		const oX = (Math.random() - 0.5) * 10;
+		const oY = (Math.random() - 0.5) * 10;
+		const oZ = (Math.random() - 0.5) * 10;
 		offsets.push(oX, oY, oZ);
 		// offsets.push(0.0, 0.0, 0.0);
 		tmpV4.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 ).normalize();
@@ -59,49 +59,47 @@ const InstancedParticles = () => {
 		varying vec2 vUv;
 		varying vec3 vColor;
 
-		uniform float uTime;
-		uniform float uStartTime;
+		uniform float uTimePassed;
 
-		float NOISE_SCALE = 1.0;
-		float WIND_STRENGTH = 0.02;
-		float NOISE_SPEED = 0.001;
+		float NOISE_SCALE = 0.3;
+		float WIND_STRENGTH = 0.009;
+		float NOISE_SPEED = 0.08;
 
-		// void main() {
-
-		// 	vec4 pos = texture2D(tPositions, vUv);
-
-		// 	vec2 lookUp = (pos.xy * NOISE_SCALE);
-		// 	lookUp = fract(lookUp);
-
-		// 	vec4 noise = texture2D(tPerlin, lookUp);
-		// 	pos.xyz += (noise.xyz - 0.5) * (WIND_STRENGTH * 2.0);
-		// 	pos = fract(pos);
-
-		// 	// if (pos.x < 0.0) pos.x = 1.0;
-		// 	// if (pos.y < 0.0) pos.y = 1.0;
-		// 	// if (pos.z < 0.0) pos.z = 1.0;
-
-		// 	// if (pos.x > 1.0) pos.x = 0.0;
-		// 	// if (pos.y > 1.0) pos.y = 0.0;
-		// 	// if (pos.z > 1.0) pos.z = 0.0;
-
-		// 	gl_FragColor = vec4(pos.xyz, 1.0);
-		// }
+		vec3 GRAVITY = vec3(0.0, -0.02, 0.0);
 
 		void main() {
-			float timePassed = (uTime - uStartTime) / 1000.0 * 0.1;
 			vec4 pos = texture2D(tPositions, vUv);
-			
-			vec2 lookUp = vec2(0.0, 0.0);
-			vec3 noise = texture2D(tPerlin, lookUp).rgb - 0.5;
 
-			vec3 GRAVITY = vec3(0.0, noise.r * 0.1, 0.0);
+			float x = fract((pos.x * NOISE_SCALE) + (uTimePassed * NOISE_SPEED) + pos.z);
+			float y = fract((pos.y * NOISE_SCALE) + (uTimePassed * NOISE_SPEED));
 			
-			pos += vec4(GRAVITY, 0.0);
-			pos = fract(pos);
+			vec2 lookUp = vec2(x, y);
+			vec3 noise = texture2D(tPerlin, lookUp).rgb - 0.5;
+			float weight = texture2D(tOrigins, lookUp).a;
+
+			vec3 wind = vec3(normalize(noise) * WIND_STRENGTH);
+
+			vec3 acceleration = vec3(0.0);
+			acceleration += GRAVITY;
+			acceleration += wind;
+			acceleration *= weight;
+			
+			pos += vec4(acceleration, 0.0);
+
+			// if (pos.y <= 0.0) pos = texture2D(tOrigins, vUv);
+			if (pos.y <= 0.0) {
+				vec3 origin = texture2D(tOrigins, vUv).xyz;
+				pos.x = origin.x;
+				pos.y = 1.0;
+				pos.z = origin.z;
+			}
+
+			pos.x = fract(pos.x);
+			pos.z = fract(pos.z);
+
+			// pos = fract(pos);
 
 			gl_FragColor = vec4(pos.xyz, 1.0);
-			// gl_FragColor = vec4(timePassed, timePassed, timePassed, 1.0);
 		}
 	`;
 
@@ -135,16 +133,12 @@ const InstancedParticles = () => {
 
 			vColor = data.xyz;
 
-			gl_Position = projectionMatrix * modelViewMatrix * vec4(  vPosition + particlePosition, 1.0 );
+			gl_Position = projectionMatrix * modelViewMatrix * vec4(  vPosition + particlePosition + offset, 1.0 );
 		}
 	`;
 
 	const fragmentShader = `
 		precision highp float;
-
-		uniform float uTime;
-		uniform float uStartTime;
-		uniform float uBlah;
 
 		varying vec3 vPosition;
 		varying vec3 vColor;
@@ -152,11 +146,7 @@ const InstancedParticles = () => {
 		float TIME_TRANSITION = 10000.0;
 
 		void main() {
-			float timePassed = uTime - uStartTime;
-			// float timePassed = 5000.0;
-			float val = timePassed / TIME_TRANSITION;
-
-			gl_FragColor = vec4(fract(uBlah), fract(uBlah), fract(uBlah), 1.0);
+			gl_FragColor = vec4(1.0);
 		}
 	`;
 
@@ -169,10 +159,10 @@ const InstancedParticles = () => {
 			data[stride] = Math.random() * 255;
 			data[stride + 1] = Math.random() * 255;
 			data[stride + 2] = Math.random() * 255;
-			data[stride + 3] = 1;
+			data[stride + 3] = (1 - (Math.random() * 0.3)) * 255; // store the weight in the origin texture
 		}
 
-		const originsTexture = new THREE.DataTexture(data, SIZE, SIZE);
+		const originsTexture = new THREE.DataTexture(data, SIZE, SIZE, THREE.RGBAFormat);
 		originsTexture.minFilter = THREE.NearestFilter;
 		originsTexture.magFilter = THREE.NearestFilter;
 		originsTexture.generateMipmaps = false;
@@ -183,15 +173,16 @@ const InstancedParticles = () => {
 			magFilter: THREE.NearestFilter,
 			format: THREE.RGBAFormat,
 			type: THREE.UnsignedByteType,
-			depthBuffer: false,
+			depthBuffer: true,
 			stencilBuffer: false,
+			transparent: true,
 		});
 
 		const renderTarget2 = renderTarget1.clone();
 		const copyShader = new GPGPU.CopyShader();
 
 		gpgpu.pass(copyShader.setTexture(originsTexture).material, renderTarget1);
-		const perlinTexture = new THREE.TextureLoader().load('/assets/textures/100-to-0-512.png');
+		const perlinTexture = new THREE.TextureLoader().load('/assets/textures/perlin-512.png');
 		perlinTexture.minFilter = perlinTexture.magFilter = THREE.NearestFilter;
 
 		console.log(perlinTexture);
@@ -205,8 +196,7 @@ const InstancedParticles = () => {
 				tPositions: { type: 't', value: positionsTexture },
 				tOrigins: { type: 't', value: originsTexture },
 				tPerlin: { type: 't', value: perlinTexture },
-				uTime: { type: 'f', value: 0.0 },
-				uStartTime: { type: 'f', value: parseFloat(Date.now()) },
+				uTimePassed: { value: 0.0 },
 			},
 			vertexShader: vertexSimulationShader,
 			fragmentShader: fragmentSimulationShader,
@@ -232,11 +222,8 @@ const InstancedParticles = () => {
 
 	const createMesh = (geometry, positionSimulationTexture) => {
 		const uniforms = {
-			uTime: { type: 'f', value: 0 },
-			uStartTime: { type: 'f', value: parseFloat(Date.now()) },
 			color: { type: 'c', value: new THREE.Color(0x3db230) },
 			tPositions: { type: 't', value: positionSimulationTexture },
-			uBlah: { type: 'f', value: 0.0 },
 		};
 
 		const material = new THREE.RawShaderMaterial({
@@ -253,12 +240,7 @@ const InstancedParticles = () => {
 
 	const update = () => {
 		const secsPast = (Date.now() - startTime) / 1000;
-
-		simulationMaterial.uniforms.uTime.value = parseFloat(Date.now());
-		mesh.material.uniforms.uTime.value = parseFloat(Date.now());
-		mesh.material.uniforms.uBlah.value = secsPast * 0.1;
-		simulationMaterial.needsUpdate = true;
-		mesh.material.needsUpdate = true;
+		simulationMaterial.uniforms.uTimePassed.value = secsPast;
 		// console.log((simulationMaterial.uniforms.uTime.value - simulationMaterial.uniforms.uStartTime.value) / 1000);
 
 
@@ -288,7 +270,7 @@ const InstancedParticles = () => {
 
 	const debugMesh = new THREE.Mesh(
 		new THREE.PlaneGeometry( 512, 512 ),
-		new THREE.MeshBasicMaterial({ map: renderTarget1, side: THREE.DoubleSide, transparent: true }),
+		new THREE.MeshBasicMaterial({ map: renderTarget1.texture, side: THREE.DoubleSide, transparent: true }),
 		// new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 	);
 	mesh.add(debugMesh);
